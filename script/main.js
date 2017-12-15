1cda9f143c0aea0284687af0fe4b588511d8a768
Cem <22315@ma-web.nl>
2017-12-15 10:16:32 +0100

    first commit

---
+++ script/main.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('button');
+let block1 = document.querySelector('.block1');
+let block2 = document.querySelector('.block2');
+let block3 = document.querySelector('.block3');
+
+const move = () => {
+  block1.classList.toggle('block1--move--out');
+  block2.classList.toggle('block2--move--out');
+  block3.classList.toggle('block3--move--out');
+}
+button.addEventListener('click', move);
